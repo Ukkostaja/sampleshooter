cd2b73980bce1ccb41d9b3105dea4d2759ef49a2
Ukkostaja <Ukkostaja@iki.fi>
2013-10-13 13:30:32 +0300

    mene

---
+++ src/com/sampleshooter/TargetArea.java
@@ -0,0 +1,32 @@
+package com.sampleshooter;
+
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class TargetArea {
+	Vector2 sijainti;
+	Rectangle hitbox;
+	
+	public TargetArea(Vector2 sijainti) {
+		this.sijainti = sijainti;
+		this.hitbox = new Rectangle(sijainti.x, sijainti.y, Art.target[0][0].getRegionWidth(), Art.target[0][0].getRegionHeight());
+			
+		
+	}
+			
+	
+	public boolean check(ArrayList<Kakka> kuulat){
+		for(Kakka kk : kuulat) {
+			
+		}
+	}
+		
+	
+	
+	public TargetArea(int x, int y){
+		this.sijainti = new Vector2(x,y);
+	}
+}
