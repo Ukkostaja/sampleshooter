ba60045c34dc99c6dc9c843f1a65a2fbd2138922
Petri Partanen <petri.m.partanen@student.jyu.fi>
2013-10-13 15:48:58 +0300

    Beatfix again!

--- src/com/sampleshooter/GameEngine.java
+++ src/com/sampleshooter/GameEngine.java
@@ -192,13 +192,16 @@ public class GameEngine extends Engine {
 		
 		// Tempo!
 		if(tempoSignal) {
+			beatAtSecond = !beatAtSecond;
+			
 			// Spawn under
 			spawn(level.getPattern());
 			
 			spawnNotes(level.getNotes());
 			
 			// Play sound
-			Sound.pulse.play();
+			if(beatAtSecond)
+				Sound.pulse.play();
 			
 			// Check for a game ending condition
 			if(level.end())
@@ -246,6 +249,8 @@ public class GameEngine extends Engine {
 	
 	// Level objects
 	private int position = -1;		// current level tempo position
+	
+	boolean beatAtSecond = false;
 	
 	// Tempo object
 	float nextTempo = 0.0f; // time for next tempo
